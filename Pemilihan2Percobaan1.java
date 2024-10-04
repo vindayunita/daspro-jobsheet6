473a59e9d4defe79188c60c894ba7b6ed322201d
vindayunita <vindasari7684@gmail.com>
2024-10-04 11:17:56 +0700

    vinda tahun kabisat

---
+++ Pemilihan2Percobaan1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan1 {
+
+    public static void main(String[] args) {
+        Scanner input24 = new Scanner(System.in);
+
+        System.out.println("Masukkan Tahun");
+        int tahun = input24.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        }
+    }
+}
